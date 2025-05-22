c760d856e3f72cd45ddc2d11d83c1c71dfbad023
Leonardo Seminatore <l02.seminatore@gmail.com>
2025-05-22 13:06:18 +0200

    add initial folder structure

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 import geopandas as gpd
 import streamlit as st
 
+from pages import statistics
+from pages import search
+
 st.write("Hello World")
+search.title()
+statistics.title()
+
+source = gpd.read_file("data/100058.csv")
---
+++ pages/statistics.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+def title():
+    st.write("Statistik")
